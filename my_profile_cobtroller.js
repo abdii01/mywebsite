6c5e3c3d43573ec1a08991a9538f498020cf54ae
Abdibouh1 <abnbbr@gmail.com>
2021-02-26 14:12:31 +0100

    first commit

---
+++ my_profile_cobtroller.js
@@ -0,0 +1,15 @@
+
+
+function showout_my_view(my_view_id){
+     var my_viiew_id=document.getElementById(my_view_id);
+     
+     my_viiew_id.style.visibility="visible";
+
+}
+
+function break_out_my_view(my_view_id){
+   var my_viiew_id=document.getElementById(my_view_id);
+   
+   my_viiew_id.style.visibility="hidden";
+
+}
